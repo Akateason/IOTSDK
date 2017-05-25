569abaa8254938b76aa0bdb954d9cb99786392d0
teason23 <akateason23@gmail.com>
2017-05-25 15:22:27 +0800

    Initial Commit

---
+++ PhicommIOTSDK/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhicommIOTSDK
+//
+//  Created by teason23 on 2017/5/25.
+//  Copyright © 2017年 teaason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PhicommIOTSDK/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PhicommIOTSDK
+//
+//  Created by teason23 on 2017/5/25.
+//  Copyright © 2017年 teaason. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
